e8a98d8dd7bf679dafef3b60bb435bc8e8e05a6c
Richard Mahn <rich.mahn@unfoldingword.org>
2025-06-05 13:02:00 -0400

    Adds Github banner

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,29 @@ function App() {
           <TSVUploadWidget />
           <ConverterResultsComponent />
         </Box>
+        <Box sx={{ padding: 2 }}>
+          <div>
+            <p>
+              This app is designed to help you clean up TSV files for Translation Notes. It allows you to upload a TSV file, select the Bible book and branch, and then process the
+              file to remove unwanted lines and ensure proper formatting.
+            </p>
+            <p>
+              For more information, view the{' '}
+              <a href="https://github.com/unfoldingWord/tsv7-tn-cleanup-app#readme" target="_blank" rel="noopener noreferrer">
+                GitHub repository's README
+              </a>
+              .
+            </p>
+            <p>
+              This may work for other TSV files with a Quote or OrigQuote column, but no guarrantee. If you'd like to ONLY convert GL Quotes to Greek/Hebrew Quotes for other
+              resources, like Study Notes or Translation Questions, visit{' '}
+              <a href="https://tsv-quote-converters.netlify.app/" target="_blank" rel="noopener noreferrer">
+                the Styleguidist App here
+              </a>{' '}
+              which are the Javascript functions that do the quote conversion.
+            </p>
+          </div>
+        </Box>
       </AppContentProvider>
     </ThemeProvider>
   );
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -8,27 +8,6 @@ const AppHeader = () => {
   return (
     <Typography variant="h4" sx={{ backgroundColor: '#38ADDF', color: '#FFF', padding: 1, textAlign: 'center' }}>
       TSV TN Cleanup App{server != 'PROD' ? ` (${server})` : ''}
-      <a
-        href="https://github.com/unfoldingWord/tsv7-tn-cleanup-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          position: 'absolute',
-          top: '25px',
-          right: '-25px',
-          backgroundColor: 'darkcyan',
-          color: '#fff',
-          padding: '8px 16px',
-          fontSize: '12px',
-          textDecoration: 'none',
-          transform: 'rotate(45deg)',
-          transformOrigin: 'center',
-          zIndex: 1000,
-          borderRadius: '0 0 0 4px',
-        }}
-      >
-        Code on GitHub
-      </a>
     </Typography>
   );
 };
